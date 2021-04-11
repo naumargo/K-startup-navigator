5122a09a445f319ac373144b5f375be0f0f480aa
TTTanya <32129133+TTTanya@users.noreply.github.com>
2021-04-11 21:14:10 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    message = models.TextField(max_length=1000)
+
+    def __str__(self):
+        # Будет отображаться следующее поле в панели администрирования
+        return self.email
